74aa574f0c5723816740a07bba1e75b64ee17b09
Anderson Freitas <freitaslimaanderson@gmail.com>
2024-03-26 22:19:08 -0300

    Script for show Pair and multiple of 5

--- ex07/lib/even_and_multiple_of_five.rb
+++ ex07/lib/even_and_multiple_of_five.rb
@@ -1,3 +1,21 @@
 def check_number(number)
-
+  if number % 2 == 0 && number % 5 == 0 
+    return "O número é par. O número é múltiplo de 5."
+   elsif number % 2 == 0
+    return "O número é par. O número não é múltiplo de 5."
+   elsif number % 5 == 0 
+    return "O número é ímpar. O número é múltiplo de 5."
+   else
+    return "O número é ímpar. O número não é múltiplo de 5."
+   end
 end
+
+=begin
+ 
+
+  return "O número é par. O número não é múltiplo de 5." if number % 2 == 0 && number % 5 != 0
+  return "O número é ímpar. O número não é múltiplo de 5." if number % 2 != 0 && number % 5 != 0
+  return "O número é par. O número é múltiplo de 5." if number % 2 == 0 && number % 5 == 0
+
+  "O número é par. O número não é múltiplo de 5."
+=end
--- ex07/lib/main.rb
+++ ex07/lib/main.rb
@@ -3,7 +3,8 @@ require_relative 'typing'
 
 def main
   number = typing
-  check_number(number)
+  result = check_number(number)
+  puts "#{number} #{result}"
 end
 
 main
